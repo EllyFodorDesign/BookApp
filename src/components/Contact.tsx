3208fcae87c8c2ddae1d9d0fa1d3b4fc9b93554b
Ellinor <ellyfodor@gmail.com>
2025-09-15 12:32:08 +0200

    Fixed contact form for phone.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,13 +6,13 @@ const ContactSection = styled.section`
 `;
 
 const Container = styled.div`
-  max-width: 1200px;
+  max-width: 100%;
   margin: 0 auto;
   padding: 0 1rem;
 `;
 
 const Content = styled.div`
-  max-width: 64rem;
+  max-width: 100%;
   margin: 0 auto;
 `;
 
@@ -39,6 +39,7 @@ const Description = styled.p`
 
 const Grid = styled.div`
   display: grid;
+
   gap: 2rem;
 
   @media (min-width: 768px) {
@@ -54,13 +55,6 @@ const Card = styled.div`
 
 const CardHeader = styled.div`
   padding: 1.5rem 1.5rem 0;
-`;
-
-const CardTitle = styled.h3`
-  font-size: 1.25rem;
-  font-weight: 600;
-  color: hsl(85, 15%, 25%);
-  margin: 0 0 0.5rem 0;
 `;
 
 const CardDescription = styled.p`
@@ -79,7 +73,7 @@ const Form = styled.form`
 `;
 
 const InputRow = styled.div`
-  display: grid;
+  display: column;
   grid-template-columns: 1fr 1fr;
   gap: 1rem;
 `;
@@ -140,97 +134,34 @@ const Button = styled.button`
   }
 `;
 
-const InfoCard = styled(Card)`
-  height: fit-content;
-`;
-
-const InfoContent = styled.div`
-  padding: 1.5rem;
-`;
-
-const InfoTitle = styled.h3`
-  font-size: 1.125rem;
-  font-weight: 600;
-  color: hsl(85, 15%, 25%);
-  margin: 0 0 0.5rem 0;
-`;
-
-const InfoText = styled.p`
-  color: hsl(85, 8%, 45%);
-  margin: 0;
-  line-height: 1.6;
-`;
-
 const Contact = () => {
   return (
     <ContactSection id="Contact">
       <Container>
         <Content>
           <Header>
-            <Title>Get In Touch</Title>
-            <Description>
-              Ready to start your next project? Let's discuss how we can help
-              you achieve your goals.
-            </Description>
+            <Title>Hör av dig</Title>
+            <Description></Description>
           </Header>
 
           <Grid>
             <Card>
               <CardHeader>
-                <CardTitle>Send us a message</CardTitle>
-                <CardDescription>
-                  Fill out the form and we'll get back to you as soon as
-                  possible.
-                </CardDescription>
+                <CardDescription></CardDescription>
               </CardHeader>
               <CardContent>
                 <Form>
                   <InputRow>
-                    <Input placeholder="First name" />
-                    <Input placeholder="Last name" />
+                    <Input placeholder="Förnamn" />
+                    <Input placeholder="Efternamn" />
                   </InputRow>
-                  <Input placeholder="Email address" type="email" />
-                  <Input placeholder="Subject" />
-                  <Textarea placeholder="Your message" />
-                  <Button type="submit">Send Message</Button>
+                  <Input placeholder="Email" type="email" />
+                  <Input placeholder="Ämne" />
+                  <Textarea placeholder="Ditt meddelande" />
+                  <Button type="submit">Skicka</Button>
                 </Form>
               </CardContent>
             </Card>
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "1.5rem",
-              }}
-            >
-              <InfoCard>
-                <InfoContent>
-                  <InfoTitle>Email</InfoTitle>
-                  <InfoText>hello@bitjo.com</InfoText>
-                </InfoContent>
-              </InfoCard>
-
-              <InfoCard>
-                <InfoContent>
-                  <InfoTitle>Phone</InfoTitle>
-                  <InfoText>+1 (555) 123-4567</InfoText>
-                </InfoContent>
-              </InfoCard>
-
-              <InfoCard>
-                <InfoContent>
-                  <InfoTitle>Address</InfoTitle>
-                  <InfoText>
-                    123 Business Street
-                    <br />
-                    Suite 100
-                    <br />
-                    City, State 12345
-                  </InfoText>
-                </InfoContent>
-              </InfoCard>
-            </div>
           </Grid>
         </Content>
       </Container>
