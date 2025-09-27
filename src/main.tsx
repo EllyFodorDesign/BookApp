91a47d0134ca6e56163935751ec272c7e885f7c5
Ellinor <ellyfodor@gmail.com>
2025-09-27 10:50:45 +0200

    Main has the theme wrapped around app.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import theme from "./theme";
+import { ThemeProvider } from "styled-components"; // Import ThemeProvider
+import React from "react";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+// Wrap App with ThemeProvider to provide the theme to all styled-components
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
