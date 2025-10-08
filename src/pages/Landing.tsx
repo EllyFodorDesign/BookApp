67b0616cb377d491b2833d0a251d4261799132f1
EllyFodorDesign <ellyfodor@gmail.com>
2025-10-08 10:56:34 +0200

    added image of book cover

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import { HeaderSection, MainSection } from "../styled-components";
 import Hero from "../components/Hero"
 import { Heading } from "../components/styled-components/texts";
+import { styled } from "styled-components";
+import omslag from "../assets/omslag.jpg";
+
+
+
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  border-radius: 0.5rem;
+`;
+
 
 const Landing: React.FC = () => (
   <>
@@ -10,7 +23,10 @@ const Landing: React.FC = () => (
     </HeaderSection>
     <MainSection>
       <Heading>Kommer snart</Heading>
-  
+ 
+  <Image src={omslag} alt="Boken" />
+
+
     
     </MainSection>
   </>
