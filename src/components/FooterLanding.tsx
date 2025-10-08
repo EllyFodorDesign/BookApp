674ef87df158e708ad8a10a31a7227adfde8b0a4
Ellinor <ellyfodor@gmail.com>
2025-10-08 17:25:38 +0200

    footer content better aligned.

--- src/components/FooterLanding.tsx
+++ src/components/FooterLanding.tsx
@@ -3,13 +3,21 @@ import styled from "styled-components";
 const StyledFooterLanding = styled.footer`
   padding: ${({ theme }) => theme.spacing.M} 0;
   border-top: 1px solid rgba(0, 0, 0, 0.04);
-  text-align: center;
+  text-align: left;
+  margin-left: ${({ theme }) => theme.spacing.M};
   color: ${({ theme }) => theme.colors.muted};
   font-size: 0.95rem;
+
+  @media (min-width: 768px) {
+  margin-left: ${({ theme }) => theme.spacing.XXL};
+  }
+
 `;
 
 export default function FooterLanding() {
   return (
-    <StyledFooterLanding>© 2025 Ellinor Fodor Johansson. Alla rättigheter reserverade.</StyledFooterLanding>
+    <StyledFooterLanding>
+      © 2025 Elly Fodor. Alla rättigheter reserverade.
+    </StyledFooterLanding>
   );
 }
