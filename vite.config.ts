8add54e0123d3b56f4083e1cb26f19b0a8c3d1ee
Ellinor <ellyfodor@gmail.com>
2025-10-10 12:31:19 +0200

    added content to spruce up the layout.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
         configFile: false
       }
 })
-  ]
+  ],
+  resolve: {
+    alias: {'@': path.resolve(__dirname, 'src') }
+  }
 })
