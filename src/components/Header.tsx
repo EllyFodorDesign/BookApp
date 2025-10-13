f322b77780c60559318cd67ad6b658fce01134d8
Ellinor <ellyfodor@gmail.com>
2025-10-13 14:35:25 +0200

    Nöjd med layout. Tog bort max width på header text.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,46 +3,44 @@ import { HeaderSection } from "../styled-components";
 
 const HeaderLanding = styled.header`
   background: ${({ theme }) => theme.colors.heroBackground};
-  padding: ${({ theme }) => theme.spacing.M} ${({ theme }) => theme.spacing.S}; /* vertical | horizontal */
+  padding: ${({ theme }) => theme.spacing.XXS};
+  ${({ theme }) => theme.spacing.XXS}; /* vertical | horizontal */
   border-bottom: 1px solid rgba(0, 0, 0, 0.04);
-  display: inline-block;
+  display: flex;
+  flex-direction: column;
   width: 100%;
   text-align: left;
-
-  @media (min-width: 768px) {
-  }
 `;
 
 export const HeaderText = styled.section`
   background: ${({ theme }) => theme.colors.heroBackground};
-text-align:left;
+  text-align: left;
   justify-content: center;
-  max-width: ${({ theme }) => theme.sizes.M};
-  margin: ${({ theme }) => theme.spacing.M} auto;
+  margin: ${({ theme }) => theme.spacing.S} auto;
 
   h1 {
     color: ${({ theme }) => theme.colors.landing};
     font-size: ${({ theme }) => theme.fontSizes.H1};
-  background: ${({ theme }) => theme.colors.heroBackground};
-  margin-top: 0;
+    background: ${({ theme }) => theme.colors.heroBackground};
+    margin-top: 0;
   }
-  h2{
-  font-size: ${({ theme }) => theme.fontSizes.H2};
-  color: ${({ theme }) => theme.colors.landingDarker};
-  max-width: 95%;
-  font-weight: normal;
+  h2 {
+    font-size: ${({ theme }) => theme.fontSizes.H2};
+    color: ${({ theme }) => theme.colors.landingDarker};
+    font-weight: normal;
   }
 
+  @media (min-width: 480px) {
+  }
 `;
-
 
 export default function Header() {
   return (
     <HeaderSection>
       <HeaderLanding>
         <HeaderText aria-labelledby="Info-om-bokrelease">
-        <h1>Kommer snart</h1>
-        <h2>Bok om elever i skolan</h2>
+          <h1>Kommer snart</h1>
+          <h2>Bok om elever i skolan</h2>
         </HeaderText>
       </HeaderLanding>
     </HeaderSection>
