6c9a2e4b43d279c0ec8d8a56d35dff2b427866a8
EllyFodorDesign <ellyfodor@gmail.com>
2025-10-17 13:15:24 +0200

    unusued paths removed.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Projects from "./components/Projects";
 import Books from "./components/Books";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
-import Test from "./pages/Test";
+
 
 const App: React.FC = () => {
   return (
@@ -20,7 +20,6 @@ const App: React.FC = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-           <Route path="/hometest" element={<Test />} />
           <Route path="/about" element={<About />} />
           <Route path="/books" element={<Books />} />
           <Route path="/contact" element={<Contact />} />
