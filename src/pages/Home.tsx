04406d4f38a0615410336ecb561e2c874709fbce
EllyFodorDesign <ellyfodor@gmail.com>
2026-02-06 09:45:44 +0100

    removed buy button temporary.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HeaderSection, MainSection } from "../styled-components";
 import Hero from "../components/Hero";
-import { ButtonGroup, Button } from "../components/Button";
+import { ButtonGroup} from "../components/Button";
 import ScheduleButton from "../components/ScheduleButton";
 import { Heading } from "../components/styled-components/texts";
 
@@ -13,7 +13,7 @@ const Home: React.FC = () => (
     <MainSection>
       <Heading>"Att leva som barn med NPF i skolan"</Heading>
       <ButtonGroup>
-        <Button>Köp boken genom att trycka här</Button>
+       {/* <Button>Köp boken genom att trycka här</Button>*/}
         <ScheduleButton buttoText="Läs mer om böckerna här" />
       </ButtonGroup>
     </MainSection>
