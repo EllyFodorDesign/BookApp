a4dc6d44494c84886b9bcc12b7065d5cea645579
EllyFodorDesign <ellyfodor@gmail.com>
2026-02-06 10:42:56 +0100

    added password to access digital copy

---
+++ src/components/PasswordGate.tsx
@@ -0,0 +1,41 @@
+import  { useState } from "react";
+import type { ReactNode } from "react";
+
+function PasswordGate({ children }: { children: ReactNode }) {
+  const [ok, setOk] = useState(
+    sessionStorage.getItem("ok") === "true"
+  );
+  const [pw, setPw] = useState("");
+
+
+  if (ok) return children;
+
+
+  return (
+    <div style={{ height: "100vh", display: "grid", placeItems: "center" }}>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            if (pw === import.meta.env.VITE_PASSWORD) {
+              sessionStorage.setItem("ok", "true");
+              setOk(true);
+            }
+          }}
+        >
+          Enter
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PasswordGate;
+
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import theme from "./theme";
 import { ThemeProvider } from "styled-components"; // Import ThemeProvider
+import PasswordGate from "./components/PasswordGate"; // Import PasswordGate component
 import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <PasswordGate>
         <App />
+        </PasswordGate>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
